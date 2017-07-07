c4befed5f9b3ac663a7b42192992078fe9216318
Sam Alsmadi <sam.alsmadi@gmail.com>
2017-07-07 15:27:54 -0400

    Initial commit

---
+++ src/com/intellimed/action/TutorialAction.java
@@ -0,0 +1,9 @@
+package com.intellimed.action;
+
+public class TutorialAction {
+	public String execute(){
+		System.out.println(">>> Hello from execute()");
+		//return "failure";
+		return "success";
+	}
+}
