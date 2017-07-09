ce6f9a3105d311032fa805f2a489aa9ac34a9bd0
Sam Alsmadi <sam.alsmadi@gmail.com>
2017-07-09 09:45:04 -0400

    Add a login page

---
+++ src/com/intellimed/action/LoginAction.java
@@ -0,0 +1,32 @@
+package com.intellimed.action;
+
+public class LoginAction {
+
+	private String userId;
+	private String password;
+	
+	public String execute(){
+		if (getUserId().equals("userId") && getPassword().equals("password")){
+			return "success";
+		}
+		return "failure";
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
